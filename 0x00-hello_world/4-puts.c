621debbcd3bc103f14d76b40d289910bd2162724
Iamerick25 <erickchinyem@gmail.com>
2023-08-10 12:46:49 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+/* This is C Programming */
+#include <stdio.h>
+/**
+ * main - Entry
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+
+	return (0);
+}
