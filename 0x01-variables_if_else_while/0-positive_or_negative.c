60126f9cf85dcd212a8568a507ade3da77cc5a74
Iamerick25 <erickchinyem@gmail.com>
2023-08-11 18:04:02 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,32 @@
+/* This is a C Programming for 0-positive_or_negative.c */
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - Entry point for program
+ *
+ *Return: the value 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("% is negative\n", n);
+	}
+
+	return (0);
+}
