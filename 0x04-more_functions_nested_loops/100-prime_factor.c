f73db4b707ad7125e70b7f6cbb504b2499262a25
Iamerick25 <erickchinyem@gmail.com>
2023-08-18 12:26:58 -0700

    Functions, nested loops

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -10,10 +10,17 @@ int main(void)
 {
 	unsigned long int i, n = 612852475143;
 
-	for (i = 3; 1 < 782849; i = i + 2)
+	while (n % 2 == 0)
 	{
-		while ((n % i == 0) && (n != i))
+		n = n / 2;
+	}
+
+	for (i = 3; i * i <= n; i = i + 2)
+	{
+		while (n % i == 0)
+		{
 			n = n / i;
+		}
 	}
 
 	printf("%lu\n", n);
