78f79d3e9b06077065f5ff7a09066de8a0971b62
Iamerick25 <erickchinyem@gmail.com>
2023-08-24 03:48:03 -0700

    ADVANCED

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -11,8 +11,20 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i = 0, num1 = 0, num2 = 0, sum = 0, carry = 0;
-	while (i <= l1 || i <= l2)
+	int l1, l2, tmpl, rl, i, sum, num1, num2, carry;
+	char tmp[10000];
+
+	rl = i = l1 = l2 = sum = num1 = num2 = carry = 0;
+	while (n1[l1] != '\0')
+	l1++;
+	while (n2[l2] != '\0')
+	l2++;
+	if (l1 + 2 > size_r || l2 + 2 > size_r)
+		return (0);
+	l1--;
+	l2--;
+
+	for (i = 0; i <= l1 || i <= l2; i++)
 	{
 		num1 = num2 = 0;
 		if (i <= l1)
@@ -28,7 +40,27 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		else
 			carry = 0;
 		r[i] = sum + '0';
-		i++;
+		rl++;
 	}
-	return carry;
+
+	if (carry > 0)
+	{
+		r[i] = carry + '0';
+		r[i + 1] = '\0';
+	}
+	tmpl = 0;
+	for (i = 0; i <= rl; i++)
+	{
+		tmp[i] = r[rl - i];
+		tmpl++;
+	}
+	for (i = 0; i < tmpl; i++)
+	{
+		if (r[i] == '\0')
+		{
+			break;
+		}
+		r[i] = tmp[i];
+	}
+	return (r);
 }
