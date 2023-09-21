b3f2060a1a7fc5258f2149aba07e335582ed05f4
Iamerick25 <erickchinyem@gmail.com>
2023-09-21 08:25:15 -0700

    Add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -25,10 +25,6 @@ list_t *add_node(list_t **head, const char *str)
 	}
 
 	new->str = strdup(str);
-	if (new->str == NULL)
-	{
-		return (NULL);
-	}
 	new->len = strlen(str);
 	new->next = *head;
 	*head = new;
