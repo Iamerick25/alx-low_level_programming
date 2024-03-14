89bf22ac15ccd030497062b2f4987e56a98e117e
Iamerick25 <erickchinyem@gmail.com>
2024-03-14 16:11:45 -0700

    Done

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,7 +6,7 @@
 #include <stdlib.h>
 
 /**
- *
+ * struct listint_s - singly linked lists
  * @n: Integer
  * @index: Index of the node in the list
  * @next: Pointer to the next node
@@ -15,9 +15,9 @@
  */
 typedef struct listint_s
 {
-		int n;
-		size_t index;
-		struct listint_s *next;
+	int n;
+	size_t index;
+	struct listint_s *next;
 } listint_t;
 
 /**
@@ -32,10 +32,10 @@ typedef struct listint_s
  */
 typedef struct skiplist_s
 {
-				int n;
-				size_t index;
-				struct skiplist_s *next;
-				struct skiplist_s *express;
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
 } skiplist_t;
 
 int linear_search(int *array, size_t size, int value);
